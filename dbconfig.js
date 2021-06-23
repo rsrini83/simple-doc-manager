35f1b97330ee2554589b9c5297f370c886d41617
rsrini83 <srinivas.itb@gmail.com>
2021-06-23 13:42:05 +0530

    Use kubectl environment variablels

--- dbconfig.js
+++ dbconfig.js
@@ -3,8 +3,8 @@ var mysql = require('mysql');
 var pool  = mysql.createPool({
   connectionLimit : 10,
   queueLimit: 100,
-  host : process.env.DB_HOST || '127.0.0.1',
-  port : process.env.DB_PORT || 3306,
+  host : process.env.MYSQL_SERVICE_HOST || process.env.DB_HOST || '127.0.0.1',
+  port : process.env.MYSQL_SERVICE_PORT || process.env.DB_PORT || 3306,
   user : process.env.DB_USER || 'root',
   password : process.env.DB_PASSWORD || 'root',
   database : process.env.DB_NAME || 'testdb',
